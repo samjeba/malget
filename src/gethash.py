4b0a5395f184f81c1ecb1f42ff75e4187118bc00
Samuel Jebamani Sargunam <samuel.jebamani@gmail.com>
2018-12-19 22:46:43 +0530

    separated md5

--- src/gethash.py
+++ src/gethash.py
@@ -2,6 +2,6 @@ import urllib.request
 
 response = urllib.request.urlopen('https://feodotracker.abuse.ch/downloads/malware_hashes.txt')
 filehash = response.read().decode('utf-8')
-
-
-print (filehash)
+filedata = filehash.split('\r\n')
+for dfile in filedata:
+    print(dfile)
